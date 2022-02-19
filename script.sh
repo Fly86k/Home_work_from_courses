d87e62aa3bf8c9c90bfbd422e8cb296c61323ed4
Igor_Fil <98766234+Fly86k@users.noreply.github.com>
2022-02-19 19:03:43 +0300

    script

---
+++ script.sh
@@ -0,0 +1,9 @@
+$ nano script.sh
+#!bin/bash
+cd git
+mkdir folder1 folder2 folder3
+cd /d/QA_training/git
+touch git file_1.txt file_2.txt file_3.txt j_file_1.json j_file_2.json
+mkdir folder1 folder2 folder3
+ls -la
+mv /d/Qa_training/Git/folder_1 file_1.txt file_2.txt /d/Qa_training/Git
